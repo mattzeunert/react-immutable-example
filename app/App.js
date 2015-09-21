c63f9c86a1bf7914a35c28214c7aaab67817e22a
Matt Zeunert <matt@mostlystatic.com>
2015-09-21 21:25:27 +0100

    Add new todos to the list via input.

--- app/App.js
+++ app/App.js
@@ -33,10 +33,22 @@ var App = React.createClass({
 		return (
 			<div>
         {todos}
+        <form onSubmit={(e) => this.onSubmit(e)}>
+          <input type="text" ref="input"/>
+        </form>
       </div>
 		);
-	}
+	},
+  onSubmit: function(e){
+    e.preventDefault();
+    var input = React.findDOMNode(this.refs.input);
+    var todoTitle = input.value;
+    input.value = "";
 
+    var todos = this.state.todos;
+    todos.push({title: todoTitle})
+    this.setState(this.state); // We're mutating the original todos list, so we just need to tell react that the state has changed.
+  }
 });
 
 module.exports = App;
